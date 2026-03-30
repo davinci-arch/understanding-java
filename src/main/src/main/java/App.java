81d62ead21f8cb7692520f1d448fba3450e9653c
davinchi-arch <parfesa.a15@gmail.com>
2026-03-30 22:30:30 +0200

    first commit
    # Conflicts:
    #	.gitignore

---
+++ src/main/src/main/java/App.java
@@ -0,0 +1,25 @@
+package main.java;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Hello world!
+ *
+ */
+/*1. Калькулятор
+        (типи даних, методи, switch, виключення)
+   - +, −, *, /
+           - перевірка ділення на 0
+        - історія обчислень*/
+public class App 
+{
+
+
+    public static void main( String[] args )
+    {
+
+    }
+}
