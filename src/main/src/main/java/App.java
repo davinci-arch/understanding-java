0aa878e725dd4682b01634db15a9577f568b8dc8
davinchi-arch <parfesa.a15@gmail.com>
2026-03-30 23:03:53 +0200

    trying PR

--- src/main/src/main/java/App.java
+++ src/main/src/main/java/App.java
@@ -1,19 +1,6 @@
 package main.java;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
-/**
- * Hello world!
- *
- */
-/*1. Калькулятор
-        (типи даних, методи, switch, виключення)
-   - +, −, *, /
-           - перевірка ділення на 0
-        - історія обчислень*/
 public class App 
 {
 
