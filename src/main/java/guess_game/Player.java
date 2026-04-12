2ca2677776124667839d23f7cd3e101d5dee272a
davinchi-arch <parfesa.a15@gmail.com>
2026-04-13 00:59:50 +0200

    [feat]: provide hint functionality

--- src/main/java/guess_game/Player.java
+++ src/main/java/guess_game/Player.java
@@ -1,7 +1,42 @@
 package guess_game;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
-public record Player(String nickname, Long globalScore, Map<UUID, Long> gamesHistory) {
+public class Player {
+    private String nickname;
+    private Long globalScore;
+    private final Map<UUID, Long> gamesHistory = new HashMap<>();
+
+    public Player(String nickname, Long globalScore) {
+        this.nickname = nickname;
+        this.globalScore = globalScore;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public Long getGlobalScore() {
+        return globalScore;
+    }
+
+    public void updateGameHistory (UUID idGame, Long score) {
+        gamesHistory.put(idGame, score);
+    }
+
+    public void setGlobalScore(Long globalScore) {
+        this.globalScore = globalScore;
+    }
+
+    public Map<UUID, Long> getGamesHistory() {
+        return gamesHistory.entrySet().stream()
+                .collect(Collectors.toUnmodifiableMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
 }
--- src/main/java/guess_game/RandomGuessGame.java
+++ src/main/java/guess_game/RandomGuessGame.java
@@ -2,10 +2,7 @@ package guess_game;
 
 import guess_game.exceptions.MissMatchRequiredFormat;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -21,22 +18,57 @@ public class RandomGuessGame {
 
     public void playGame(Player player, GameLevel chosenLevel) {
         var points = 0L;
+        var hintMaxCount = 1;
+        var hintCounts = 0;
+        var requiredPoints = chosenLevel.pointsForWinning;
         for(int i = 0; i < AMOUNT_OF_TRIES; i++) {
             var guessingNumber = generateRandomNumber(chosenLevel);
             var answer = scanner.nextLine();
+            var hintRequiernmentAnswer = "";
+
             if (isAnswerValid(answer)) {
                 if (Integer.parseInt(answer) == guessingNumber) {
                     points += chosenLevel.pointsForWinning;
                     break;
                 } {
-                    //TODO: need minor fix
-                    System.out.printf("U have %d left", (AMOUNT_OF_TRIES - points));
+                    System.out.printf("U have %d attempts left", (AMOUNT_OF_TRIES - (i + 1)));
+                    System.out.println("Do you require a hint? y/n");
+                    hintRequiernmentAnswer = scanner.nextLine();
+                    if (hintRequiernmentAnswer.trim().equalsIgnoreCase("y") && hintCounts < hintMaxCount) {
+                        System.out.println("Open first number? y/n");
+                        hintRequiernmentAnswer = scanner.nextLine();
+                        if (hintRequiernmentAnswer.trim().equalsIgnoreCase("y")) {
+                            var currentAmountOfPoints = player.getGlobalScore();
+                            if (currentAmountOfPoints < requiredPoints) {
+                                System.out.println("Sorry, but u don't have enough points");
+                            } else {
+                                provideComplexHint(guessingNumber);
+                                player.setGlobalScore(currentAmountOfPoints - requiredPoints);
+                            }
+                        } else {
+                            provideSimpleHint(guessingNumber, answer);
+                        }
+                        hintCounts++;
+                    }
+                    System.out.println(hintCounts);
                 }
             } else {
                 throw new MissMatchRequiredFormat(String.format("Your answer \"%s\" do not match the format", answer));
             }
         }
         score.put(player, points);
+    }
+
+    private void provideComplexHint(int guessingNumber) {
+        String modifiedNumber = String.valueOf(guessingNumber).substring(0, 1);
+        String replacer = "*".repeat(String.valueOf(guessingNumber).length() - 1);
+        System.out.println("Guessing number: " + modifiedNumber + replacer);
+    }
+
+    private void provideSimpleHint(int guessingNumber, String answer) {
+        String hint = Integer.parseInt(answer) > guessingNumber ?
+                "ur answer is above targeted" : "ur answer is below targeted";
+        System.out.println(hint);
     }
 
     private boolean isAnswerValid(String answer) {
