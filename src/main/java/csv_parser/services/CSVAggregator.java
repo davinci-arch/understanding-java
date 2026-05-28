f518740a7cc0a957f34d90a8c1381613ec4e813e
davinchi-arch <parfesa.a15@gmail.com>
2026-05-28 16:49:16 +0200

    [feat]: aggregation layer

---
+++ src/main/java/csv_parser/services/CSVAggregator.java
@@ -0,0 +1,45 @@
+package csv_parser.services;
+
+import csv_parser.model.CSVData;
+
+import java.util.List;
+
+public class CSVAggregator {
+
+    double getAverage(List<CSVData> data, String columnName) {
+        var sumOfNumbers = data.stream()
+                .map(v -> Integer.parseInt(v.getRowData().get(columnName)))
+                .reduce(0, Integer::sum);
+
+        return (double) sumOfNumbers / data.size();
+    }
+
+    double getMax(List<CSVData> data, String columnName) {
+        return data.stream()
+                .map(v -> Double.parseDouble(v.getRowData().get(columnName)))
+                .reduce(0.0, Double::max);
+    }
+
+    double getMin(List<CSVData> data, String columnName) {
+        return data.stream()
+                .map(v -> Double.parseDouble(v.getRowData().get(columnName)))
+                .reduce(0.0, Double::min);
+    }
+
+    double getSum(List<CSVData> data, String columnName) {
+        return data.stream()
+                .map(v -> Double.parseDouble(v.getRowData().get(columnName)))
+                .reduce(0.0, Double::sum);
+    }
+    long count(List<CSVData> data, String columnName) {
+        return data.stream()
+                .map(v -> v.getRowData().get(columnName))
+                .count();
+    }
+    long countThatIsNotEmpty(List<CSVData> data, String columnName) {
+        return data.stream()
+                .map(v -> v.getRowData().get(columnName))
+                .filter(v -> !v.isBlank())
+                .count();
+    }
+}
